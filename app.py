6477486830df608ab7a0d1dd88b66421b260ea18
Noah <noah@noahball.com>
2024-06-17 14:13:30 +1200

    Log out route

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ app.add_url_rule('/', 'home_page', view_func=pages.home_page)
 app.add_url_rule('/category/<cat_id>', 'category_page', view_func=pages.category_page)
 app.add_url_rule('/login', 'login_page', methods=['GET', 'POST'], view_func=pages.login_page)
 app.add_url_rule('/signup', 'signup_page', methods=['GET', 'POST'],view_func=pages.signup_page)
+app.add_url_rule('/logout', 'logout_page', view_func=pages.logout_page)
 
 if __name__ == '__main__':
     app.run()
