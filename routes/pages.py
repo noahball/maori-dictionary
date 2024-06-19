4a75cce75c79917b1e0cb1710feba33b183f60bd
Noah <noah@noahball.com>
2024-06-20 09:18:04 +1200

    Display cat names and word/category error handling

--- routes/pages.py
+++ routes/pages.py
@@ -5,7 +5,7 @@ This file contains our routes for viewing categories and words.
 """
 
 # Import the necessary modules
-from flask import render_template, redirect, session
+from flask import render_template, redirect, session, request
 from lib import globals, db, helpers
 
 
@@ -26,13 +26,26 @@ def home_page():
 
     categories = helpers.get_categories()  # Retrieve all categories from the database for the sidebar
     # Render the home page, passing the words and categories to the template
-    return render_template('home.html', words=words, categories=categories, session=session)
+    return render_template('home.html', words=words, categories=categories, session=session, error=request.args.get("error"), cat_name="All Words")
 
 
 # /category/<cat_id> (Category page) route
 def category_page(cat_id):
     if not helpers.user_authenticated():  # Check if the user is logged in
         return redirect('/login')  # If not, redirect to the login page
+
+    # Check if the category id provided is valid and grab the category name
+    query = "SELECT name FROM category WHERE id = ?"  # Query to retrieve the category name
+    conn = db.create_connection(globals.DATABASE_FILE)  # Create a connection to the database
+    cur = conn.cursor()  # Create a cursor object
+    cur.execute(query, (cat_id,))  # Execute the query
+    cat_name = cur.fetchone()  # Retrieve the category name
+    conn.close()  # Close the connection
+
+    if cat_name is None:  # If the category id is invalid
+        return redirect('/?error=Category+not+found')  # Redirect to the home page
+    else:
+        cat_name = cat_name[0] # Retrieve the category name from the tuple
 
     # Retrieve all words from the database that match the category id provided
     # Query to retrieve all words in a category
@@ -44,8 +57,9 @@ def category_page(cat_id):
     conn.close()  # Close the connection
 
     categories = helpers.get_categories()  # Retrieve all categories from the database for the sidebar
+
     # Render the category page, passing the words in the category and sidebar categories to the template
-    return render_template('home.html', words=words, categories=categories)
+    return render_template('home.html', words=words, cat_name=cat_name, categories=categories)
 
 
 # /word/<word_id> (Word details page) route
@@ -62,6 +76,19 @@ def word_page(word_id):
     word = cur.fetchone()  # Retrieve the word
     conn.close()  # Close the connection
 
+    if word is None:  # If the word id is invalid
+        return redirect('/?error=Word+not+found')
+
+    # Get the category name for the word
+    query = "SELECT name FROM category WHERE id = ?"  # Query to retrieve the category name
+    conn = db.create_connection(globals.DATABASE_FILE)  # Create a connection to the database
+    cur = conn.cursor()  # Create a cursor object
+    cur.execute(query, (word[5],))  # Execute the query
+    cat_name = cur.fetchone()  # Retrieve the category name
+    conn.close()  # Close the connection
+
+    cat_name = cat_name[0]  # Retrieve the category name from the tuple
+
     categories = helpers.get_categories()  # Retrieve all categories from the database for the sidebar
     # Render the word details page, passing the word and sidebar categories to the template
-    return render_template('word.html', word=word, categories=categories)
+    return render_template('word.html', word=word, cat_name=cat_name, categories=categories)
