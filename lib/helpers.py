547f8dbac019a92cddfd7b28c10ddfb15f024a01
Noah <noah@noahball.com>
2024-06-19 14:39:50 +1200

    Validation helpers

--- lib/helpers.py
+++ lib/helpers.py
@@ -6,6 +6,7 @@ This file contains our helper functions.
 
 # Import the necessary modules
 from flask import session
+import re
 
 from lib import db, globals  # Import our database functions and global environment variables
 
@@ -32,3 +33,26 @@ def user_authenticated():
         The user's id if they are logged in, otherwise None
     """
     return 'user_id' in session
+
+
+def validate_string_length(field: str, min_length: int, max_length: int):
+    """ Validate a string based on its length
+    Parameters:
+        field (str): The field to validate
+        min_length (int): The minimum length of the field
+        max_length (int): The maximum length of the field
+    Returns:
+        True if the field is valid, otherwise False
+    """
+    return min_length <= len(field) <= max_length
+
+
+def validate_string_regex(field: str, regex: str):
+    """ Validate a string based on a regular expression
+    Parameters:
+        field (str): The field to validate
+        regex (str): The regular expression to match against
+    Returns:
+        True if the field is valid, otherwise False
+    """
+    return re.match(regex, field) is not None
